75904b6d065bd3390eb03b2d0e2b6fd8564b9779
Divyansh044 <divyanshbangwal@gmail.com>
2024-02-01 23:38:55 +0530

    Adding login and Signup authentication

--- ecommerce/divauth/urls.py
+++ ecommerce/divauth/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from divauth import views
 urlpatterns = [
-    path("signup/",views.signup,name='signup')
+    path("signup/",views.signup,name='signup'),
+    path("login/",views.login,name='login')
 ]
--- ecommerce/divauth/views.py
+++ ecommerce/divauth/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+
+#email import
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core import mail
+from django. core.mail.message import EmailMessage
 
 # Create your views here.
 def signup(request):
+
+    if request.method=="POST":
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['pass1']
+        confirm_password=request.POST['pass2']
+        if password!=confirm_password:
+            messages.warning(request,"Password doest not match")              
+            return render(request,'auth/signup.html')
+        try:
+            if User.objects.get(email=email):
+                messages.warning(request,"Email is already Used")
+                return render(request,'auth/signup.html')
+        
+        except Exception as identifier:
+            pass
+
+        
+        myuser=User.objects.create_user(username,email,password)
+        myuser.save()
+        messages.info(request,"Signup Successfull!!! Please Login")
+        return redirect('/auth/login/')
+    
     return render(request,'auth/signup.html')
+
+
+def login(request):
+    return render(request,'auth/login.html')
