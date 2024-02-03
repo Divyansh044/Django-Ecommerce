543ac580e74a5dcc822ad3023069f8e73fdbfcad
Divyansh044 <divyanshbangwal@gmail.com>
2024-02-03 23:52:38 +0530

    Successfully added signup and login features

--- ecommerce/divauth/views.py
+++ ecommerce/divauth/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse,redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate,login as auth_login,logout
 from django.contrib import messages
 
 #email import
@@ -13,7 +13,6 @@ from django. core.mail.message import EmailMessage
 def signup(request):
 
     if request.method=="POST":
-        username=request.POST['username']
         email=request.POST['email']
         password=request.POST['pass1']
         confirm_password=request.POST['pass2']
@@ -21,21 +20,38 @@ def signup(request):
             messages.warning(request,"Password doest not match")              
             return render(request,'auth/signup.html')
         try:
-            if User.objects.get(email=email):
+            if User.objects.get(username=email):
                 messages.warning(request,"Email is already Used")
                 return render(request,'auth/signup.html')
         
         except Exception as identifier:
             pass
+        
+        except Exception as identifier:
+            pass
 
         
-        myuser=User.objects.create_user(username,email,password)
+        myuser=User.objects.create_user(email,email,password)
         myuser.save()
         messages.info(request,"Signup Successfull!!! Please Login")
-        return redirect('/auth/login/')
+        return redirect('/divauth/login/')
     
     return render(request,'auth/signup.html')
 
 
 def login(request):
+    if request.method=="POST":
+        username=request.POST['email']
+        userpassword=request.POST['pass1']
+        myuser=authenticate(username=username,password=userpassword)
+
+        if myuser is not None:
+            auth_login(request,myuser)
+            messages.success(request,'Login Success')
+            return render(request,'index.html')
+        
+        else:
+            messages.error(request,"Email or password is wrong")
+            return redirect('/divauth/login')
+
     return render(request,'auth/login.html')
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -115,7 +116,9 @@ STATIC_URL = "static/"
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
-
+MESSAGE_TAGS={
+    messages.ERROR:'danger'
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
